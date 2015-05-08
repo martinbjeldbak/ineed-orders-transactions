8b4e2e806d815f02b653634ce7ce97352d9dcefc
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2015-05-08 14:03:51 -0700

    Move Order to models folder and have it implement a new interface for interacting with team 10's DB.

--- iterations/3-(2015-05-08)/models/Order.php
+++ iterations/3-(2015-05-08)/models/Order.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '../vendor/autoload.php';
 
 class Order {
     private $paymentType, $vendorId, $total, $tax, $dealId, $dealDiscount;
---
+++ iterations/3-(2015-05-08)/models/iDBModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace models;
+
+
+interface iDBModel {
+    public function commit();
+}
